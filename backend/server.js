46b83a2e5e84a47a4f2a5ae3159421b713a6bb88
sangmesh-18 <131734721+sangmesh-18@users.noreply.github.com>
2024-11-17 15:48:42 +0530

    1st part completed - backend

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser';
+
+import authRoute from './routes/auth.route.js'
+import messageRoute from './routes/message.route.js'
+import userRoute from "./routes/user.route.js"
+
+import connectDB from './db/db.js';
+
+
+
+const app = express();
+const port=process.env.PORT || 4000;
+
+dotenv.config();
+
+app.use(express.json());
+app.use(cookieParser());
+
+
+app.use("/api/auth",authRoute)
+app.use("/api/message",messageRoute)
+app.use("/api/user",userRoute)
+
+
+app.get('/',(req,res)=>{
+    res.send('Hello, World!')
+})
+
+
+app.listen(port,()=>{
+    connectDB()
+    console.log(`Server is running on port ${port}`);
+})
