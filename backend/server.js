3f0a20de5cf5e939762dbf33d3b559d0d4e97587
sangmesh-18 <131734721+sangmesh-18@users.noreply.github.com>
2024-11-19 13:57:41 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 import authRoute from './routes/auth.route.js'
 import messageRoute from './routes/message.route.js'
@@ -14,6 +15,8 @@ import { app, server } from './socket/socket.js';
 
 const port=process.env.PORT || 4000;
 
+const __dirname =path.resolve();
+
 dotenv.config();
 
 app.use(express.json());
@@ -24,6 +27,11 @@ app.use("/api/auth",authRoute)
 app.use("/api/message",messageRoute)
 app.use("/api/user",userRoute)
 
+ app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+ app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))  // serve the frontend build as static file
+ })
 
 app.get('/',(req,res)=>{
     res.send('Hello, World!')
